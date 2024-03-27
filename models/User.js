98bf1637a6b2c2515526c002c68a5588c2ee30b0
KpSuphakorn <suphakorn04413@gmail.com>
2024-03-27 19:37:30 +0700

    fig user can see password

--- models/User.js
+++ models/User.js
@@ -33,7 +33,7 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please add a password'],
         minlength: 6,
-        select: false
+        // select: false
     },
     resetPasswordToken: String,
     resetPasswordExpire: Date,
